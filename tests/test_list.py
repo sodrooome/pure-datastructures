9dcf94a654b7bbce340e2abd78f67ffc78d8bacb
sodrooome <15523163@students.uii.ac.id>
2020-05-25 07:43:48 +0700

    writing test for list

--- tests/test_list.py
+++ tests/test_list.py
@@ -1,6 +1,6 @@
 import unittest
 
-from ds.ds_list import OrderedList, UnorderedList
+from ds.ds_list import OrderedList, UnorderedList, DoubleLinkedList
 
 class ListTestCase(unittest.TestCase): 
     def test_ordered_list(self):
@@ -26,6 +26,18 @@ class ListTestCase(unittest.TestCase):
 
         # expand size an array
         self.assertEqual(3, olist.procedure_expand_size())
+
+    def test_double_linkedlist(self):
+        olist = DoubleLinkedList()
+        olist.procedure_insert(1)
+        olist.procedure_insert(3)
+        olist.procedure_insert(5)
+
+        # test if inserting items in beginning is work
+        self.assertTrue(True, olist.procedure_insert_start(51))
+
+        # test for searching an object
+        self.assertEqual(True, olist.procedure_search_obj(1))
 
 if __name__ == "__main__":
     unittest.main()
